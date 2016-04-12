d6766460399e53930056b1d7b011e08a039333d9
AnatolyKorablyov <=>
2016-04-12 22:57:37 +0300

    чуть-чуть переименований

---
+++ lab_2_graphz/lab_2_graphz/test.bat
@@ -0,0 +1,21 @@
+rem @echo off
+SET program="%1"
+if %program% == "" goto err
+
+echo 1
+%program% "input.txt" "output.txt"
+if NOT ERRORLEVEL 1 goto testFailed
+
+echo 2
+%program% "input1.txt" "output2.txt"
+if NOT ERRORLEVEL 1 goto testFailed
+
+echo OK
+exit /B
+
+:testFailed
+echo Testing failed
+exit /B
+
+:err
+echo Usage: test.bat <Path to program>
